afb9fc220084c0a10058695fe878e5080db2c7d8
KitlerUA <volodymyr.kit.ua@gmail.com>
2017-12-20 18:02:47 +0200

    Added local check for forecast

--- weather/weather.go
+++ weather/weather.go
@@ -8,11 +8,54 @@ import (
 
 	"context"
 
+	"reflect"
+
+	"strconv"
+
 	"github.com/KitlerUA/WeatherForecastBot/config"
 	"github.com/KitlerUA/WeatherForecastBot/db"
 	"github.com/olivere/elastic"
 	"github.com/yanzay/log"
 )
+
+const indexMapping = `
+{
+	"settings":{
+		"number_of_shards": 1,
+		"number_of_replicas": 0
+	},
+	"mappings":{
+		"info":{
+			"properties":{
+				"location":{
+					"type":"long"
+				},
+				"temp":{
+					"type":"float"
+				},
+				"humidity":{
+					"type":"float"
+				},
+				"description":{
+					"type":"text",
+					"store": true,
+					"fielddata": true
+				},
+				"dttxt":{
+					"type":"long"
+				}
+			}
+		}
+	}
+}`
+
+type InfoElastic struct {
+	Location    int
+	Temp        float32
+	Humidity    float32
+	Description string
+	DtTxt       int64
+}
 
 type Info struct {
 	Main struct {
@@ -43,7 +86,7 @@ func Get(startDate, endDate time.Time) string {
 		log.Fatalf("Cannot check index: %s", err)
 	}
 	if !exists {
-		createIndex, err := db.Get().CreateIndex("wetbot").Do(ctx)
+		createIndex, err := db.Get().CreateIndex("wetbot").BodyString(indexMapping).Do(ctx)
 		if err != nil {
 			log.Fatalf("Cannot create index: %s ", err)
 		}
@@ -51,8 +94,12 @@ func Get(startDate, endDate time.Time) string {
 			log.Fatal("Not acknowledge")
 		}
 	}
-	query := elastic.NewRangeQuery("Date").From(startDate.Format("2006-01-02 15:04:05")).To(endDate.Format("2006-01-02 15:04:05")).Boost(3)
-	searchResult, err := db.Get().Search().Index("wetbot").Query(query).Do(ctx)
+	query := elastic.NewRangeQuery("DtTxt").Gte(startDate.Unix()).Lte(endDate.Unix())
+	src, _ := query.Source()
+	data, err := json.Marshal(src)
+	got := string(data)
+	log.Printf("Query %s", got)
+	searchResult, err := db.Get().Search().Index("wetbot").Type("info").Query(query).Do(ctx)
 	if err != nil {
 		log.Fatalf("Cannot search: %s", err)
 	}
@@ -63,55 +110,32 @@ func Get(startDate, endDate time.Time) string {
 			log.Fatalf("Cannot get weather from OpenMap: %s", err)
 		}
 		for _, info := range weather.List {
-			marshaled, err := json.Marshal(&info)
-			if err != nil {
-				log.Fatalf("Cannot marshal info: %s", err)
-			}
 
-			//TODO: Error 400 (Bad Request): failed to parse [type=mapper_parsing_exception]
-			_, err = db.Get().Index().Index("wetbot").Type("wetinfo").BodyJson(marshaled).Do(ctx)
+			eInfo := infoToElasticInfo(info)
+			_, err := db.Get().Index().Index("wetbot").Type("info").BodyJson(eInfo).Do(ctx)
 			if err != nil {
-				log.Fatalf("Cannot put: %s", err)
+				log.Fatalf("Cannot put %s: %s", eInfo, err)
 			}
+			//log.Printf("Puted %s, %s, %s", put.Id, put.Index, put.Type)
 		}
+		_, err = db.Get().Flush().Index("wetbot").Do(ctx)
 	}
-	searchResult, err = db.Get().Search().Index("wetbot").Query(query).Do(ctx)
+	//log.Printf("Query %s", query)
+	searchResult, err = db.Get().Search().Index("wetbot").Type("info").Query(query).Do(ctx)
+	log.Printf("Search result len %s", searchResult.TotalHits())
 	if err != nil {
 		log.Fatalf("Cannot search: %s", err)
 	}
-	replyString := ""
-	for _, v := range searchResult.Hits.Hits {
-		log.Printf("Reply string = %s", replyString)
-		replyString += v.Fields["weather"].(string)
-	}
-	//get forecast from server, if we have no local data
-	/*weatherClient := http.Client{
-		Timeout: 5 * time.Second,
-	}
-	req, err := http.NewRequest(http.MethodGet, "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9ebbdc484f058b6e91cba224d761fea2", nil)
-	if err != nil {
-		return err.Error()
-	}
-	res, err := weatherClient.Do(req)
-	if err != nil {
-		return err.Error()
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return err.Error()
-	}
-	weather := InfoList{}
-	if err = json.Unmarshal(body, &weather); err != nil {
-		return err.Error()
-	}
-	replyString := ""
-	for _, i := range weather.List {
-		replyString += i.DtTxt + " "
-		replyString += i.Weather[0].Description + "; "
-		replyString += "Temperature " + strconv.Itoa(int(i.Main.Temp)) + "°C ; Humidity " + strconv.Itoa(int(i.Main.Humidity)) + "%\n"
-	}
 
-	return replyString*/
+	replyString := ""
+	var info InfoElastic
+	//log.Printf("Search ", searchResult.Hits.Hits)
+	for _, item := range searchResult.Each(reflect.TypeOf(info)) {
+		if t, ok := item.(InfoElastic); ok {
+			replyString += time.Unix(t.DtTxt, 0).Format("2006-01-02 15:04:05") + " " + t.Description + " " + strconv.Itoa(int(t.Temp)) + "°C " + strconv.Itoa(int(t.Humidity)) + "%\n"
+		}
+	}
+	//_, _ = db.Get().DeleteIndex("wetbot").Do(ctx)
 	return replyString
 }
 
@@ -120,7 +144,7 @@ func getWeatherFromOpenMap() (InfoList, error) {
 		Timeout: 5 * time.Second,
 	}
 	weather := InfoList{}
-	req, err := http.NewRequest(http.MethodGet, "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9ebbdc484f058b6e91cba224d761fea2", nil)
+	req, err := http.NewRequest(http.MethodGet, "http://api.openweathermap.org/data/2.5/forecast?id=524901&units=metric&appid=9ebbdc484f058b6e91cba224d761fea2", nil)
 	if err != nil {
 		return weather, err
 	}
@@ -135,4 +159,16 @@ func getWeatherFromOpenMap() (InfoList, error) {
 	err = json.Unmarshal(body, &weather)
 	return weather, err
 
+}
+
+func infoToElasticInfo(info Info) InfoElastic {
+	dt, _ := time.Parse("2006-01-02 15:04:05", info.DtTxt)
+	res := InfoElastic{
+		Location:    702550,
+		Temp:        info.Main.Temp,
+		Humidity:    info.Main.Humidity,
+		Description: info.Weather[0].Description,
+		DtTxt:       dt.Unix(),
+	}
+	return res
 }
